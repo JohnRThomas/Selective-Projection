ad57b052e48d3195ae6b5fcac4ca5ba931f7f7b6
John Thomas <TheLimitInfinity@hotmail.com>
2015-05-28 16:01:29 -0400

    added comments to boot main.

--- selpro/boot/Main.java
+++ selpro/boot/Main.java
@@ -10,24 +10,32 @@ import selpro.frames.ProjectionFrame;
 public class Main {
 
 	public static void main(String args[]){
+		//Set the window theming
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
 
+		//open up a connection to an available webcam
 		CameraReader camera = new CameraReader();	
+		
+		//Create the three frames
 		ProjectionFrame projectionFrame = new ProjectionFrame("Projector Output");
 		DisplayFrame displayFrame = new DisplayFrame("Camera");
 		ControlFrame controlFrame = new ControlFrame("Control Panel", projectionFrame, displayFrame);
+		
+		//Set where the camera callback send it's frames
 		camera.addFrameListener(controlFrame);
-		controlFrame.setVisible(true);
+		
+		//Set where the projector frame will draw from
+		projectionFrame.setDrawer(controlFrame);
 
+		//Show the frames and move them to their proper locations reletive to each other.
+		controlFrame.setVisible(true);
 		displayFrame.setLocation(controlFrame.getWidth()+controlFrame.getX(), controlFrame.getY());
 		displayFrame.setVisible(true);
-
 		projectionFrame.setLocation(displayFrame.getWidth()+displayFrame.getX(), displayFrame.getY());
-		projectionFrame.setDrawer(controlFrame);
 		projectionFrame.setVisible(true);
 
 	}
